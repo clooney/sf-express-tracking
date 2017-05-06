7574866cd2091c0a268f9fdb46e9f386bcfaacff
clooney <phpjd@qq.com>
2017-05-06 19:24:04 +0800

    sf express tracking api
    sf express tracking api

---
+++ delete-a-tracking-item.php
@@ -0,0 +1,7 @@
+<?php
+
+include("track.class.php");
+$track = new Trackingmore;
+$track = $track->deleteTrackingItem('sf-express','RM131516216CN');
+
+?>
---
+++ get-tracking-results-of-a-single-tracking.php
@@ -0,0 +1,7 @@
+<?php
+
+include("track.class.php");
+$track = new Trackingmore;
+
+$track = $track->getSingleTrackingResult('sf-express','LK166607403CN');
+?>
